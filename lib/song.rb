b86a6ff460ccd96697316c59e5bc70704ec03aec
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-06 20:09:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,6 @@ class Song
     song = self.new(filename.split(" - ")[1])
     song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
     song.artist.save
-    song.artist.add_song(song)
     song
   end
 
