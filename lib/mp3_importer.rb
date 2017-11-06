4e52c9976ede1592b7efd5f612177bce508e0a57
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-06 19:39:13 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,7 @@ class MP3Importer
   end
 
   def files
-    Dir[@path].select{ |f| File.file? f }.map{ |f| File.basename f }
+    Dir.entries(@path).select{ |f| File.file? f }
   end
 
   def import
