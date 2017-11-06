b6a23a6ece2a7b780089251e6dd3f916bd40ea88
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-06 19:49:07 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,12 +4,12 @@ class MP3Importer
   attr_accessor :path, :files
 
   def initialize(path)
-    @path = path
+    @path = path << "/*"
     @files = []
   end
 
   def files
-    Dir[@path/*].each { |filename| @files << filename }
+    Dir[@path].each { |filename| @files << filename }
     binding.pry
   end
 
